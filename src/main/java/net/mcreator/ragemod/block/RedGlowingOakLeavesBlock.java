3579aa1975ce2b24e877fbf019a02f34ab6f436e
CrashGyuluka <69863086+CrashGyuluka@users.noreply.github.com>
2022-02-19 15:40:40 +0100

    Glowing Vines Craft, Generation Tweaks and Fixes

--- src/main/java/net/mcreator/ragemod/block/RedGlowingOakLeavesBlock.java
+++ src/main/java/net/mcreator/ragemod/block/RedGlowingOakLeavesBlock.java
@@ -1,25 +1,24 @@
 
 package net.mcreator.ragemod.block;
 
-import net.minecraft.world.level.storage.loot.LootContext;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.item.TieredItem;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.core.BlockPos;
 
-import java.util.List;
-import java.util.Collections;
+import net.mcreator.ragemod.procedures.RedGlowingOakLeavesBlockDestroyedByPlayerProcedure;
 
 public class RedGlowingOakLeavesBlock extends LeavesBlock {
 	public RedGlowingOakLeavesBlock() {
 		super(BlockBehaviour.Properties.of(Material.LEAVES).sound(SoundType.GRASS).strength(0.25f, 2f).lightLevel(s -> 9)
-				.requiresCorrectToolForDrops().noOcclusion());
+				.requiresCorrectToolForDrops().noOcclusion().noDrops());
 		setRegistryName("red_glowing_oak_leaves");
 	}
 
@@ -36,10 +35,9 @@ public class RedGlowingOakLeavesBlock extends LeavesBlock {
 	}
 
 	@Override
-	public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
-		List<ItemStack> dropsOriginal = super.getDrops(state, builder);
-		if (!dropsOriginal.isEmpty())
-			return dropsOriginal;
-		return Collections.singletonList(new ItemStack(this, 1));
+	public boolean removedByPlayer(BlockState blockstate, Level world, BlockPos pos, Player entity, boolean willHarvest, FluidState fluid) {
+		boolean retval = super.removedByPlayer(blockstate, world, pos, entity, willHarvest, fluid);
+		RedGlowingOakLeavesBlockDestroyedByPlayerProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ(), entity);
+		return retval;
 	}
 }
---
+++ src/main/java/net/mcreator/ragemod/procedures/GreenGlowingOakLeavesBlockDestroyedByPlayerProcedure.java
@@ -0,0 +1,40 @@
+package net.mcreator.ragemod.procedures;
+
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Entity;
+
+import net.mcreator.ragemod.init.RagemodModBlocks;
+
+public class GreenGlowingOakLeavesBlockDestroyedByPlayerProcedure {
+	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
+		if (entity == null)
+			return;
+		if ((entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY).getItem() == Items.SHEARS
+				|| (entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY).getItem() == Items.SHEARS
+				|| EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SILK_TOUCH,
+						(entity instanceof LivingEntity _livEnt ? _livEnt.getMainHandItem() : ItemStack.EMPTY)) != 0
+				|| EnchantmentHelper.getItemEnchantmentLevel(Enchantments.SILK_TOUCH,
+						(entity instanceof LivingEntity _livEnt ? _livEnt.getOffhandItem() : ItemStack.EMPTY)) != 0) {
+			if (world instanceof Level _level && !_level.isClientSide()) {
+				ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(RagemodModBlocks.GREEN_GLOWING_OAK_LEAVES));
+				entityToSpawn.setPickUpDelay(10);
+				_level.addFreshEntity(entityToSpawn);
+			}
+		} else {
+			if (Math.random() < 0.05) {
+				if (world instanceof Level _level && !_level.isClientSide()) {
+					ItemEntity entityToSpawn = new ItemEntity(_level, x, y, z, new ItemStack(RagemodModBlocks.GREEN_GLOWING_OAK_TREE_SAPLING));
+					entityToSpawn.setPickUpDelay(10);
+					_level.addFreshEntity(entityToSpawn);
+				}
+			}
+		}
+	}
+}
